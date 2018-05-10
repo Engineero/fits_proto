6104f90ead0311b8f4939b09625bf4d5772830e5
Engineero <Engineero@users.noreply.github.com>
2018-05-10 15:27:42 -0600

    Began writing tests for protobuf

---
+++ tests/test_fits_proto.py
@@ -0,0 +1,23 @@
+#*******************************************************************************
+#                                UNCLASSIFIED
+#*******************************************************************************
+#*******************************************************************************
+# Filename: test_fits_proto.py
+# Language: Python
+# Author: ntoner
+# Created: 2018-05-10
+#
+# Description:
+# Tests for the FITS protobuf. Basically verify that I can translate a real
+# FITS file into my protobuf and back again without losing any of the
+# information in the original file.
+#
+#*******************************************************************************
+
+import unittest
+
+
+
+#*******************************************************************************
+#                                UNCLASSIFIED
+#*******************************************************************************
